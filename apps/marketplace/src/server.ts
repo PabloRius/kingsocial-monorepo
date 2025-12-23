e1c1176708e063be208cb2e8a325e734219a1672
PabloRius <pablogrius@gmail.com>
2025-12-23 18:11:54 +0100

    Config change test

--- apps/marketplace/src/server.ts
+++ apps/marketplace/src/server.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 
-const PORT = 4000;
+const PORT = parseInt(process.env.PORT || "3001") || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Users service running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Marketplace component running on http://localhost:${PORT}`);
 });
